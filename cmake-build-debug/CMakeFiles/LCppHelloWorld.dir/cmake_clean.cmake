1fd1257a5ebd942511f86f6cf8b8952b3f135bcb
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2021-12-15 12:27:52 +0100

    Hello World

---
+++ cmake-build-debug/CMakeFiles/LCppHelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LCppHelloWorld.dir/main.cpp.obj"
+  "CMakeFiles/LCppHelloWorld.dir/main.cpp.obj.d"
+  "LCppHelloWorld.exe"
+  "LCppHelloWorld.exe.manifest"
+  "LCppHelloWorld.pdb"
+  "libLCppHelloWorld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LCppHelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
