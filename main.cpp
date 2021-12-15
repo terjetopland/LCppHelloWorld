9dcd402e95dd5fab9983833793c94a9102d05799
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2021-12-15 12:43:30 +0100

    just a twist of Hello World

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string Hello = "Hello";
+    char comma = ',';
+    char space = ' ';
+    string World = "World";
+    char exclamationMark = '!';
+
+    string theFirstConsoleWo = { 0 };
+    cout << "Please tell what your first words in the console was\n";
+    getline(cin, theFirstConsoleWo);
+
+    if(theFirstConsoleWo.compare(Hello+comma+space+World) != true || theFirstConsoleWo.compare(Hello+comma+space+World+exclamationMark) != true )
+        cout << "That was exactly the same as me" << endl;
+    else
+
+    cout << "Really?? Mine was this.\n" << Hello << comma << space << World << exclamationMark << endl;
+
+
 
 
     return 0;
